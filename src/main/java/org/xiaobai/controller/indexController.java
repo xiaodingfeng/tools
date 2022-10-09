a4f8578920008eef5af726fd7f5bf67a69e9a6df
18179144782 <1440651649@qq.com>
2022-10-09 15:57:10 +0800

    遗漏文件

---
+++ src/main/java/org/xiaobai/controller/indexController.java
@@ -0,0 +1,22 @@
+package org.xiaobai.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class indexController {
+
+    @Value("${spring.url}")
+    private String fromUrl;
+
+   //只要能进这个页面，代表服务运行正常
+    @GetMapping("/")
+    public void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect(fromUrl);
+    }
+}
