96710c9d1a2fcdf7e71cf51c059524f742675b3b
18179144782 <1440651649@qq.com>
2022-10-18 14:42:02 +0800

    每日一文

--- src/main/java/org/xiaobai/response/MeiRiYiWen.java
+++ src/main/java/org/xiaobai/response/MeiRiYiWen.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class MeiRiYiWen {
     private String date;
+    private String title;
     private String author;
     private String digest;
     private String content;
